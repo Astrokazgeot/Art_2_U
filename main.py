dbfaff5af0606857ba705a0ff42fd6fe92ec1a6f
Astrokazgeot <sidskaz297@gmail.com>
2025-07-18 14:58:34 +0530

    Updated with checkpoint

--- main.py
+++ main.py
@@ -11,12 +11,37 @@ conv_base=ResNet50(
     include_top=False, # means remove top dense layer
     input_shape=(224,224,3) # standard for resnet 
 )
+# Load both datasets
+ds1 = tf.keras.utils.image_dataset_from_directory(
+    "test/",
+    labels='inferred',
+    label_mode='int',
+    batch_size=32,
+    image_size=(224,224)
+)
+
+ds2 = tf.keras.utils.image_dataset_from_directory(
+    "valid/",
+    labels='inferred',
+    label_mode='int',
+    batch_size=32,
+    image_size=(224,224)
+)
+
+# Combine them
+combined_ds = ds1.concatenate(ds2)
 
 model=Sequential()
 model.add(conv_base)
 model.add(Flatten())
 model.add(Dense(256,activation='relu'))
-model.add(Dense(26,activation='softmax'))  #output layer
+model.add(Dense(25,activation='softmax'))  #output layer
+
+data_augmentation = keras.Sequential([
+    keras.layers.RandomFlip("horizontal"),
+    keras.layers.RandomRotation(0.1),
+    keras.layers.RandomZoom(0.1),
+])
 
 conv_base.trainable=True
 set_trainable=False
@@ -36,25 +61,24 @@ train_ds=keras.utils.image_dataset_from_directory(
     image_size=(224,224)
 )
 
-test_ds=keras.utils.image_dataset_from_directory(
-    directory='test/',
-    labels='inferred',
-    label_mode='int',
-    batch_size=32,
-    image_size=(224,224)
-)
-
 def process(image,label):
     image=tf.cast(image/255.0,tf.float32)
     return image,label
-train_ds=train_ds.map(process)
-test_ds=test_ds.map(process)
+
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.map(process).map(lambda x, y: (data_augmentation(x, training=True), y))
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+combined_ds = combined_ds.map(process).cache().prefetch(buffer_size=AUTOTUNE)
 
 model.compile(
     optimizer='adam',
     loss='sparse_categorical_crossentropy',
     metrics=['accuracy']
 )
+from keras.callbacks import ModelCheckpoint
 
-model.fit(train_ds,epochs=10,validation_data=test_ds)
+checkpoint = ModelCheckpoint("best_model.h5", monitor='val_accuracy', save_best_only=True)
 
+model.fit(train_ds,epochs=30,validation_data=combined_ds)
